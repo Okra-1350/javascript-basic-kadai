e60ee5c073eb57359b5133b6753e7344ea37f877
Okra-1350 <166097509+Okra-1350@users.noreply.github.com>
2024-06-17 08:10:46 +0900

    ５章の課題再提出します

--- kadai-005/bmi.js
+++ kadai-005/bmi.js
@@ -1,10 +1,13 @@
 //体重
-let weight;
-weight = 68;
+let weight = 68;
 
 //身長
-let height;
-height = 1.7;
+let height = 1.7;
 
-//計算式
-console.log(weight / (height * height));
+// BMIの計算式
+let bmi = weight / (height * height);
+
+
+//BMIの計算結果の出力
+console.log(bmi);
+
